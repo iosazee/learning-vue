5ef58fe2083a830b150c272a3408c62bf1149132
Zee Mudia <iosazee1@gmail.com>
2023-08-31 13:39:00 +0100

    inputing multiple values in the data ppty

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     //data, functions
     data () {
         return {
-            title: 'The Final Destination'
+            title: 'The Final Destination',
+            author: 'Meg Ryan',
+            age: 29
         }
     }
       
